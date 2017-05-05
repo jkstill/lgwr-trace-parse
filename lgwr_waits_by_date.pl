24115f33d54b73a059a78624158453ba6fc06d3a
Jared Still <jkstill@gmail.com>
2017-05-05 13:40:20 -0700

    replaced lgwr_waits_by_date.pl with the correct script

--- lgwr_waits_by_date.pl
+++ lgwr_waits_by_date.pl
@@ -6,7 +6,6 @@
 use Data::Dumper;
 #use Data::TreeDumper;
 
-sub normalize($@);
 
 my $debug = 0;
 my $debug2 = 0;
@@ -24,79 +23,49 @@ foreach my $file (@files) {
 	-r $file || die "File $file cannot be accessed - $!\n";
 }
 
-my @lgwrTime = ();
+my %lgwrTime = ();
 my $lgwrGT=0;
 
 foreach my $file (@files) {
-	open F,'<',"$file" || die "cannot open $file - $!\n";
-	my @x=grep (/^Warning: log write elapsed time/,<F>);
-	chomp @x;
-	my ($d,$time,$size);
-	foreach my $line (@x) {
-		($d,$d,$d,$d,$d,$time,$d,$size) = split(/\s+/,$line);
-		$time =~ s/ms,//;
-		$lgwrGT = $time if $time > $lgwrGT;
-		push @lgwrTime , $time;
-	}
-	print Dumper(\@x) if $debug;	
-	
-}
 
-print Dumper(\@lgwrTime) if $debug2;
+	# ugly hack here, but in a hurry
+	open F,'-|',"/bin/grep  -B1 '^Warning: log write elapsed time' $file | grep -v -- '^--' " || die "cannot open grep - $!\n";
+	my ($d,$time,$size,$date);
+	my $isDate=1;
+	my ($dateLine,$elapsedMsg);
 
-print "Longest Wait: $lgwrGT\n" if $debug2;
+	while (<F>) { 
 
-# find first element GT longest wait and trim list
-my $lastEl=0;
-foreach my $i (0.. $#histoScales) {
+		chomp;
+		my $line=$_;
 
-	#print "$i\n";
-	$lastEl = $i;
-	last if $histoScales[$i] > $lgwrGT;
-	
-}
+		if ($isDate) {
+			$isDate = 0;
+			$dateLine = $line;
+			($d,$date) = split(/\s+/,$dateLine);
+			#print "Date: $date\n";
+		} else {
+			$isDate = 1;
+			$elapsedMsg = $line;
+			($d,$d,$d,$d,$d,$time,$d,$size) = split(/\s+/,$elapsedMsg);
+			$time =~ s/ms,//;
+			$lgwrGT = $time if $time > $lgwrGT;
+			#print "elapsed: $time\n\n";
+			$lgwrTime{$date} += $time;
+		}
 
-print "$lastEl\n" if $debug2;
-print "$histoScales[$lastEl]\n" if $debug2;
-@histoScales = @histoScales[0 .. $lastEl];
-
-print Dumper(\@histoScales) if $debug2;
-
-# now create the histogram hash
-
-my %histo = map { $_ => 0 } @histoScales;
-
-if ($debug2) {
-	print "\n\%histo\n";
-	print Dumper(\%histo);
-}
-
-# populate 
-
-foreach my $time (@lgwrTime) {
-	my $ntime = normalize($time,@histoScales);
-	print "time: $time        ntime: $ntime\n" if $debug2;
-	$histo{$ntime}++;
-}
-
-print Dumper(\%histo) if $debug2;
-
-foreach my $time ( sort  {$a <=> $b}  keys %histo ) {
-	printf  "%8s  %8d\n", $time, $histo{$time};
-}
-
-sub normalize ($@) {
-	my $time = shift @_;
-	my @scales = @_;
-	my $ntime;
-	my $lastEl;
-	foreach my $i (0 .. $#scales) {
-		$lastEl = $i;
-		last if $scales[$i] >= $time;
 	}
 
-	$scales[$lastEl];
 }
+
+
+print "Longest Wait: $lgwrGT\n";
+
+
+foreach my $date (sort keys %lgwrTime) {
+	print "$date  $lgwrTime{$date}\n";
+}
+
 
 
 
